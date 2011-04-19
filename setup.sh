eb112f3a614486f74937b6c22b497451a0482f72
Maxim Kachur <mcdebugger@duganet.ru>
2011-04-19 21:59:13 +0400

    Build scripts improvements
    Detect WIN32 builds, setup install directory variable
    setup.sh rework. Now you can use debug and windows flags together
    Use arkilloid-debug executable name for WIN32 DEBUG builds

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+for arg in "$@"; do
+	case "$arg" in
+	'--debug' | '-d' | 'debug')
+		CMAKE_RUN_FLAGS="${CMAKE_RUN_FLAGS} -DDEBUG=1"
+		;;
+	'--win32' | '--windows' | 'win32' | 'windows')
+		CMAKE_RUN_FLAGS="${CMAKE_RUN_FLAGS} \
+		-DCMAKE_TOOLCHAIN_FILE=~/dev/Toolchain-mingw32.cmake \
+		-DCMAKE_INSTALL_PREFIX=/home/mcdebugger/dev/mingw32-install/setup \
+		-DSDL_INCLUDE_DIR=/home/mcdebugger/dev/i686-mingw32/include/SDL/"
+		;;
+	*)
+		echo 'Default settings:'
+		echo 'DEBUG OFF'
+		echo 'WIN32 OFF'
+		;;
+	esac
+done
+
 if [ ! -f "extern/glftfont/GLFT_Font.cpp" ]; then
 	echo -n 'Initializing and updating required git submodules... '
 	git submodule init > /dev/null 2>&1
@@ -12,13 +31,7 @@ if [ ! -d "build" ]; then
 fi
 
 cd build
-if [ "$1" = 'debug' ]; then
-	cmake -D DEBUG=1 ..
-elif [ "$1" = 'windows' ]; then
-	cmake -DCMAKE_TOOLCHAIN_FILE=~/dev/Toolchain-mingw32.cmake -DCMAKE_INSTALL_PREFIX=/home/mcdebugger/dev/mingw32-install -DSDL_INCLUDE_DIR=/home/mcdebugger/dev/i686-mingw32/include/SDL/  ..
-else
-	cmake ..
-fi
+cmake ${CMAKE_RUN_FLAGS} ..
 make
 
 echo ''
